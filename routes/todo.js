2dd0a12cbe125a277117b43ed2219a66aac8d626
greetingsfromtw <greetingsfromtw@gmail.com>
2016-04-13 20:18:30 +0800

    let different users has different todo list

--- routes/todo.js
+++ routes/todo.js
@@ -2,25 +2,35 @@ require('../lib/db');
 
 var express = require('express'),
     mongoose = require('mongoose');
-
 var router = express.Router();
-
 var Todo = mongoose.model('Todo');
 
 //待辦事項頁面
-router.get('/todo',function(req,res,next){
-  res.render('todo/todo')
-});
+router.get('/',function(req,res,next){
+  if(!req.session.logined){
+    res.redirect('/users/login')
+  }
+  res.locals.logined= req.session.logined;
+  res.locals.accountname= req.session.accountname;
+
+  Todo.find({accountname: req.session.accountname},function ( err, todos, count ){
+      res.render('todo/todo',{
+        todos:todos
+      })
+    }) 
+  });
 
 
 //新增事項
 router.post('/add',function(req,res){
   Todo.create({
+    accountname:req.session.accountname,
     item:req.body.add
   })
   res.locals.logined= req.session.logined;
-  Todo.find(function ( err, todos, count ){
-  res.redirect('/todo/todo')
+  
+  Todo.find({accountname: req.session.accountname},function ( err, todos, count ){
+    res.redirect('/todo')
   })  
 })
 
@@ -28,20 +38,19 @@ router.post('/add',function(req,res){
 router.get('/delete/:id', function(req, res, next) {
     Todo.remove({ _id: req.params.id }, function(err) {
         if (err)
-            console.log('Fail to delete article.');
+            console.log('Fail to delete item.');
         else
             console.log('Done');
     });
     res.locals.logined= req.session.logined;
-  Todo.find(function ( err, todos, count ){
-  res.redirect( '/todo')
+  Todo.find({accountname: req.session.accountname},function ( err, todos, count ){
+  res.redirect('/todo')
   })  
 })
 
 
 //編輯事項
-router.get('todo/edit/:id', function(req, res, next) {
-  res.locals.logined= req.session.logined;
+router.get('/edit/:id', function(req, res, next) {
   Todo.find({ _id: req.params.id }, function ( err, todos, count ){
     res.render( 'todo/edit', {
       title:'todo list testing',
@@ -52,7 +61,7 @@ router.get('todo/edit/:id', function(req, res, next) {
 });
 
 //更新事項
-router.post('todo/update/:id', function(req, res, next) {
+router.post('/update/:id', function(req, res, next) {
     Todo.update({ _id: req.params.id }, { item: req.body.getitem }, function(err) {
         if (err)
             console.log('Fail to update item.');
@@ -60,7 +69,7 @@ router.post('todo/update/:id', function(req, res, next) {
             console.log('Done');
     })
     res.locals.logined= req.session.logined;
-    Todo.find(function ( err, todos, count ){
+    Todo.find({accountname: req.session.accountname},function ( err, todos, count ){
   res.redirect( '/todo')
   })   
 });
